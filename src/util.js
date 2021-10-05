0b0a8faba71f68c4017e5ecbfa298ad00b6e4efe
uhavaiya <uhavaiya@gmail.com>
2021-10-05 20:58:52 +0530

    Cases Circle Update

--- src/util.js
+++ src/util.js
@@ -7,13 +7,13 @@ const casesTypeColors = {
     hex: "#CC1034",
     rgb: "rgb(204, 16, 52)",
     half_op: "rgba(204, 16, 52, 0.5)",
-    multiplier: 800,
+    multiplier: 300,
   },
   recovered: {
     hex: "#7dd71d",
     rgb: "rgb(125, 215, 29)",
     half_op: "rgba(125, 215, 29, 0.5)",
-    multiplier: 1200,
+    multiplier: 250,
   },
   deaths: {
     hex: "#fb4443",
